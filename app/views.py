6ea44e87eccc8ed17117173e05b800e33a9a812e
chepceline <chepceline25@gmail.com>
2022-04-28 22:37:19 +0300

    add all changes

--- app/views.py
+++ app/views.py
@@ -8,6 +8,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
+    
     message = 'Hello World'
     return render_template('index.html',message = message)
 
@@ -19,3 +22,4 @@ def movie(movie_id):
     View movie page function that returns the movie details page and its data
     '''
     return render_template('movie.html',id = movie_id)
+
